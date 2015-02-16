7445f75280a7b60e084cfdaae157733507ef6cac
Josh Gough <jsgough@gmail.com>
2015-02-16 14:17:44 -0500

    Wrapped error in JSON object
    S-51177

--- src/app/apikey.js
+++ src/app/apikey.js
@@ -1,9 +1,8 @@
 (function(module) {
 	var config = require('./config');
-	// TODO: wrap in a JSON object
 	module.exports = function (req, res, next) {
     	if (!config.apiKey || !req.query.key || req.query.key != config.apiKey) {
-        	res.status(401).send('API key parameter missing or invalid');
+        	res.status(401).json({error:'API key parameter missing or invalid'});
     	} else {
         	return next();
     	}
