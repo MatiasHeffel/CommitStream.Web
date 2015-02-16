235e35191a3ae61352e966034039499ea29b5870
Josh Gough <jsgough@gmail.com>
2015-02-16 14:11:38 -0500

    Added comment for tech-debt item
    S-51177

--- src/app/apikey.js
+++ src/app/apikey.js
@@ -1,6 +1,6 @@
 (function(module) {
 	var config = require('./config');
-
+	// TODO: wrap in a JSON object
 	module.exports = function (req, res, next) {
     	if (!config.apiKey || !req.query.key || req.query.key != config.apiKey) {
         	res.status(401).send('API key parameter missing or invalid');
